881e7403c55a6bab4966f36c9ecc815d276368d9
mikosz08 <46966211+mikosz08@users.noreply.github.com>
2022-12-21 15:17:01 +0100

    adds default data

---
+++ Python/Text_Game/data/default_entity_data.py
@@ -0,0 +1,13 @@
+from data.Race import Race
+from data.CharacterClass import CharacterClass
+
+DEFAULT_NAME = f"Bob"
+DEFAULT_LEVEL = 1
+DEFAULT_HP = 100
+DEFAULT_MP = 100
+DEFAULT_ATT = 10
+DEFAULT_DEF = 10
+DEFAULT_SPD = 10
+
+DEFAULT_RACE = Race(1)
+DEFAULT_CHARACTER_CLASS = CharacterClass(1)
